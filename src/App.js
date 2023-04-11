b0a5d3e4699d3898541b84e39dd19adf1cf35ef4
everinurmind <nurbol.sultanov@gmail.com>
2023-04-10 19:52:38 -0600

    Wrap <App> with the <Provider> component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {
   BrowserRouter, Routes, Route, Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Header from './components/Header';
 import BookList from './components/BookList';
 import Categories from './components/Categories';
@@ -8,23 +10,25 @@ import './styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Header />
-        <ul>
-          <li>
-            <Link to="/books">Books</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/books" element={<BookList />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <nav>
+          <Header />
+          <ul>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/books" element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
